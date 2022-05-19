d11f9d9c2e60a2a1f10e27ad6a664b72a7c0e4db
hatz <hatz@localhost.localdomain>
2022-05-19 13:30:03 +0530

    curd operation done

---
+++ index.js
@@ -0,0 +1,123 @@
+const TOKEN = "90939035|-31949284158763881|90940535";
+const DB = "Student";
+const REL = "Student-Rel";
+
+//get user data from the jsondb
+function getdata(name) {
+  jQuery.ajaxSetup({ async: false });
+  $.post(
+    "http://api.login2explore.com:5577/api/irl",
+    JSON.stringify({
+      token: "90939035|-31949284158763881|90940535",
+      cmd: "GET_BY_KEY",
+      dbName: "Demo",
+      rel: "Demo-rel",
+      createTime: true,
+      updateTime: true,
+      jsonStr: {
+        name,
+      },
+    }),
+    (d) => {
+      const { data, status } = JSON.parse(d);
+      if (status === 400) {
+        return alert("User Not Found");
+      }
+      sessionStorage.setItem("name",name)
+      const { rec_no, record } = JSON.parse(data);
+      console.log(record);
+      if (location.pathname.slice(1) === "update.html") {
+        document.getElementById("rec").innerHTML = rec_no;
+        document.getElementById("id").value = record.id;
+        document.getElementById("name").value = record.name;
+        document.getElementById("email").value = record.email;
+        document.getElementById("mobile").value = record.mobile;
+      } else {
+        document.getElementById("usergetdata").style.display = "block";
+        document.getElementById("rec").innerHTML = rec_no;
+        document.getElementById("uid").innerHTML = record.id;
+        document.getElementById("uname").innerHTML = record.name;
+        document.getElementById("uemail").innerHTML = record.email;
+        document.getElementById("unumber").innerHTML = record.mobile;
+      }
+    }
+  ).fail((err) => console.log(err));
+  jQuery.ajaxSetup({ async: true });
+}
+
+//create new user data to jsondb
+function createdata() {
+  const id = document.getElementById("id").value;
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const mobile = document.getElementById("mobile").value;
+
+  jQuery.ajaxSetup({ async: false });
+  $.post(
+    "http://api.login2explore.com:5577/api/iml",
+    JSON.stringify({
+      token: "90939035|-31949284158763881|90940535",
+      cmd: "PUT",
+      dbName: "Demo",
+      rel: "Demo-rel",
+      jsonStr: {
+        id,
+        name,
+        email,
+        mobile,
+      },
+    }),
+    (data) => {
+      const { status } = JSON.parse(data);
+      if (status === 200) {
+        alert("user created successfully");
+        window.location = "index.html";
+      }
+    }
+  ).fail((err) => console.log(err));
+  jQuery.ajaxSetup({ async: true });
+}
+
+//update an existing data in the jsonDB
+function updatedata(num) {
+  const id = document.getElementById("id").value;
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const mobile = document.getElementById("mobile").value;
+  let dat = createUPDATERecordRequest(
+    "90939035|-31949284158763881|90940535",
+    JSON.stringify({ id, name, email, mobile }),
+    "Demo",
+    "Demo-rel",
+    num
+  );
+  jQuery.ajaxSetup({ async: false });
+  $.post("http://api.login2explore.com:5577/api/iml", dat, (data) => {
+    console.log(data);
+    alert("Data updated");
+    window.location = "index.html";
+  }).fail((err) => console.log(err));
+  jQuery.ajaxSetup({ async: true });
+}
+
+//remove the existing data form the jsondb
+function removedata(n) {
+  jQuery.ajaxSetup({ async: false });
+  $.post(
+    "http://api.login2explore.com:5577/api/iml",
+    JSON.stringify({
+      token: "90939035|-31949284158763881|90940535",
+      cmd: "REMOVE",
+      dbName: "Demo",
+      rel: "Demo-rel",
+      jsonStr: {},
+      record: n,
+    }),
+    (data) => {
+      alert("user remove successfully");
+      window.location = "index.html";
+      console.log(data);
+    }
+  ).fail((err) => console.log(err));
+  jQuery.ajaxSetup({ async: true });
+}
